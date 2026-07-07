e65e98b4959594ed291dace00e81ea4efe5cdc08
mxcury <mxcury.dev@proton.me>
2026-07-07 21:40:15 +0100

    edit brave.nix

--- modules/apps/brave.nix
+++ modules/apps/brave.nix
@@ -7,13 +7,6 @@
         "--force-dark-mode"
         "--enable-features=WebUIDarkMode"
       ];
-      extraOpts = {
-        "BraveAIChatEnabled" = false;
-        "BraveWalletDisabled" = true;
-        "BraveRewardsDisabled" = true;
-        "BraveVPNDisabled" = true;
-        "BraveNewsDisabled" = true;
-      };
 
       extensions = [
         {id = "ghmbeldphafepmbegfdlkpapadhbakde"; } # Proton Pass
