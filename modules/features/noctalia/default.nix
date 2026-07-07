86184bf7df962712678397cdc17289253b07dc45
Tom <58176147+mxcury@users.noreply.github.com>
2026-07-07 19:52:57 +0100

    Simplify settings assignment in default.nix

--- modules/features/noctalia/default.nix
+++ modules/features/noctalia/default.nix
@@ -2,10 +2,7 @@
   perSystem = { pkgs, ... }: {
     packages.noctalia-wrapped = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
       inherit pkgs;
-      settings = {
-	(builtins.fromJSON
-          (builtins.readFile ./noctalia.json)).settings;
-      };
+	  settings = (builtins.fromJSON (builtins.readFile ./noctalia.json)).settings;
     };
   };
 }
