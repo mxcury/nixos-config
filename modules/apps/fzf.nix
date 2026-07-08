4a0ac394b938341586ae40cc62d0403a64eff3a1
Tom <58176147+mxcury@users.noreply.github.com>
2026-07-08 09:19:35 +0100

    Enhance fzf configuration with additional commands

--- modules/apps/fzf.nix
+++ modules/apps/fzf.nix
@@ -1,9 +1,27 @@
 { self, inputs, ... }: {
-  flake.homeModules.fzf = { pkgs, ... }: {
+  flake.homeModules.fzf = { pkgs, lib, ... }: {
     programs.fzf = {
       enable = true;
       enableZshIntegration = true;
-      defaultOptions = [ "--height 40%" "--layout=reverse" "--border" ];
+
+      defaultCommand = "${lib.getExe pkgs.fd} --type f --hidden --exclude .git";
+      defaultOptions = [
+        "--height 40%"
+        "--layout=reverse"
+        "--border"
+        "--preview '${lib.getExe pkgs.bat} --color=always --style=numbers --line-range=:200 {}'"
+        "--preview-window=right:50%"
+      ];
+
+      fileWidgetCommand = "${lib.getExe pkgs.fd} --type f --hidden --exclude .git";
+      fileWidgetOptions = [
+        "--preview '${lib.getExe pkgs.bat} --color=always --style=numbers --line-range=:200 {}'"
+      ];
+
+      changeDirWidgetCommand = "${lib.getExe pkgs.fd} --type d --hidden --exclude .git";
+      changeDirWidgetOptions = [
+        "--preview '${lib.getExe pkgs.eza} --tree --color=always {} | head -200'"
+      ];
     };
   };
 }
