bf5abc095827aa4a0cd9c54c9a06f8a6e851d6e8
Tom <58176147+mxcury@users.noreply.github.com>
2026-07-08 12:44:10 +0100

    Add NVim configuration with plugins and settings

---
+++ modules/apps/nvf.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, inputs, ... }: {
+
+  imports = [
+    inputs.nvf.homeManagerModules.default
+  ];
+
+  programs.nvf = {
+    enable = true;
+    enableManpages = true;
+
+    settings = {
+      vim = {
+        theme.enable = false;
+
+        extraPlugins = {
+          noctalia = {
+            package = inputs.noctalia-nvim;
+            
+            setup = ''
+              require('noctalia').setup({ 
+                palette_path = vim.fn.expand("~/.config/noctalia/colors.json"),
+                auto_reload = true, -- Watches for wallpaper changes and hot-reloads Neovim live!
+                transparent = false,
+              })
+              vim.cmd.colorscheme("noctalia")
+            '';
+          };
+        };
+
+        options = {
+          number = true;
+          relativenumber = true;
+        };
+
+        statusline.lualine = {
+          enable = true;
+          theme = "auto"; # "auto" lets lualine read the colorscheme dynamic values directly
+        };
+
+        telescope.enable = true;
+
+        autocomplete.blink-cmp = {
+          enable = true;
+          friendly-snippets.enable = true;
+        };
+
+        lsp.enable = true;
+      };
+    };
+  };
+}
