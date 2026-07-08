3b4a25933b6ffe23a642dfdbe59a3bd2f54bce74
Tom <58176147+mxcury@users.noreply.github.com>
2026-07-08 16:58:20 +0100

    Add Steam configuration to Nix flake

--- modules/features/steam.nix
+++ modules/features/steam.nix
@@ -1,1 +1,14 @@
+{ self, inputs, ... }: {
+  flake.nixosModules.steam = { pkgs, ... }: {
+    nixpkgs.overlays = [ inputs.millennium.overlays.default ];
 
+    programs.steam = {
+      enable = true;
+      package = pkgs.millennium-steam;
+      gamescopeSession.enable = true;
+      remotePlay.openFirewall = true;
+    };
+
+    programs.gamemode.enable = true;
+  };
+}
