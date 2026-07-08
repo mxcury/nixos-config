2072a9c81fba544d457b4a51e4ca78eeaef1947d
mxcury <mxcury.dev@proton.me>
2026-07-08 22:36:59 +0100

    add matugen config files

--- modules/apps/nvf.nix
+++ modules/apps/nvf.nix
@@ -1,61 +1,80 @@
-{ inputs, ... }: {
-  flake.homeModules.nvf = { pkgs, config, ... }: {
-    imports = [
-      inputs.nvf.homeManagerModules.default
-    ];
+{ inputs, self, ... }: {
+  flake.homeModules.nvf =
+    {
+      pkgs,
+      config,
+      lib,
+      ...
+    }:
+    let
+      noctaliaDir = "${self}/modules/features/noctalia";
+    in
+    {
+      imports = [
+        inputs.nvf.homeManagerModules.default
+      ];
 
-    programs.nvf = {
-      enable = true;
-      settings.vim = {
-        viAlias = true;
-        vimAlias = true;
+      xdg.configFile."nvim/lua/matugen-template.lua".source = "${noctaliaDir}/matugen-template.lua";
+      xdg.configFile."noctalia/user-templates.toml".source = "${noctaliaDir}/user-templates.toml";
 
-        theme.enable = false;
+      programs.nvf = {
+        enable = true;
+        settings.vim = {
+          viAlias = true;
+          vimAlias = true;
 
-        extraPlugins = with pkgs.vimPlugins; {
-          base16-nvim = {
-            package = base16-nvim;
+          theme.enable = false;
+
+          extraPlugins = with pkgs.vimPlugins; {
+            base16-nvim = {
+              package = base16-nvim;
+            };
           };
-        };
 
-        # --- The VS Code Layout & Visuals ---
-        statusline.lualine.enable = true;
-        tabline.nvimBufferline.enable = true;
-        filetree.neo-tree.enable = true;
+          additionalRuntimePaths = [ "${config.home.homeDirectory}/.config/nvim" ];
 
-        visuals = {
-          nvim-web-devicons.enable = true;
-          nvim-cursorline.enable = true;
-        };
+          extraConfigLua = ''
+            pcall(function() require('matugen').setup() end)
+          '';
 
-        # --- IDE Features ---
-        telescope.enable = true;
-        treesitter = {
-          enable = true;
-          autotagHtml = true;
-        };
+          # --- The VS Code Layout & Visuals ---
+          statusline.lualine.enable = true;
+          tabline.nvimBufferline.enable = true;
+          filetree.neo-tree.enable = true;
 
-        autocomplete.nvim-cmp.enable = true;
-        autopairs.nvim-autopairs.enable = true;
+          visuals = {
+            nvim-web-devicons.enable = true;
+            nvim-cursorline.enable = true;
+          };
 
-        lsp = {
-          enable = true;
-          formatOnSave = true;
-          lightbulb.enable = true;
-        };
+          # --- IDE Features ---
+          telescope.enable = true;
+          treesitter = {
+            enable = true;
+            autotagHtml = true;
+          };
 
-        languages = {
-          nix.enable = true;
-          typescript.enable = true;
-          html.enable = true;
-          css.enable = true;
-        };
+          autocomplete.nvim-cmp.enable = true;
+          autopairs.nvim-autopairs.enable = true;
 
-        git = {
-          enable = true;
-          gitsigns.enable = true;
+          lsp = {
+            enable = true;
+            formatOnSave = true;
+            lightbulb.enable = true;
+          };
+
+          languages = {
+            nix.enable = true;
+            typescript.enable = true;
+            html.enable = true;
+            css.enable = true;
+          };
+
+          git = {
+            enable = true;
+            gitsigns.enable = true;
+          };
         };
       };
     };
-  };
 }
