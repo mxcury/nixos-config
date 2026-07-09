8fb2e33fe1d2cb55975e57abcc514746c8267a4e
mxcury <mxcury.dev@proton.me>
2026-07-09 17:39:32 +0100

    enable niriCursor

--- modules/apps/niriCursor.nix
+++ modules/apps/niriCursor.nix
@@ -1,6 +1,7 @@
 { _self, _inputs, ... }: {
   flake.homeModules.niriCursor = { pkgs, ... }: {
     home.pointerCursor = {
+      enable = true;
       gtk.enable = true;
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Ice";
