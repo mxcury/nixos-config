51333e0ad965982a6c1896b3dbd0ba933f52f804
mxcury <mxcury.dev@proton.me>
2026-07-09 21:16:36 +0100

    binds usable

--- modules/features/niri/_binds.nix
+++ modules/features/niri/_binds.nix
@@ -1,15 +1,19 @@
-{ lib, pkgs, self' }: {
+{
+  lib,
+  pkgs,
+  self',
+}:
+{
   ### Apps ###
   "Mod+Return".spawn-sh = lib.getExe pkgs.alacritty;
   "Mod+B".spawn-sh = lib.getExe pkgs.brave;
   "Mod+Q".close-window = _: { };
   "Mod+Space".spawn-sh = "${lib.getExe self'.packages.noctalia-wrapped} ipc call launcher toggle";
-  
+  "Mod+Tab".toggle-overview = { };
+
   ### Focus movement (arrows) ###
   "Mod+Left".focus-column-left = { };
   "Mod+Right".focus-column-right = { };
-  "Mod+Down".focus-window-down = { };
-  "Mod+Up".focus-window-up = { };
 
   ### Focus movement (vim-style HJKL, mirrors arrows) ###
   "Mod+H".focus-column-left = { };
@@ -18,23 +22,21 @@
   "Mod+K".focus-window-up = { };
 
   ### Move windows/columns ###
-  "Mod+Ctrl+Left".move-column-left = { };
-  "Mod+Ctrl+Right".move-column-right = { };
-  "Mod+Ctrl+Down".move-window-down = { };
-  "Mod+Ctrl+Up".move-window-up = { };
-  
+  "Mod+Shift+Left".move-column-left = { };
+  "Mod+Shift+Right".move-column-right = { };
+
   ### Move windows/columns (vim-style HJKL, mirrors arrows) ###
-  "Mod+Ctrl+H".move-column-left = { };
-  "Mod+Ctrl+L".move-column-right = { };
-  "Mod+Ctrl+J".move-window-down = { };
-  "Mod+Ctrl+K".move-window-up = { };
-  
+  "Mod+Shift+H".move-column-left = { };
+  "Mod+Shift+L".move-column-right = { };
+  "Mod+Shift+J".move-window-down = { };
+  "Mod+Shift+K".move-window-up = { };
+
   ### Workspaces (scroll-style) ###
-  "Mod+Page_Down".focus-workspace-down = { };
-  "Mod+Page_Up".focus-workspace-up = { };
-  "Mod+Ctrl+Page_Down".move-column-to-workspace-down = { };
-  "Mod+Ctrl+Page_Up".move-column-to-workspace-up = { };
-  
+  "Mod+Down".focus-workspace-down = { };
+  "Mod+Up".focus-workspace-up = { };
+  "Mod+Shift+Down".move-column-to-workspace-down = { };
+  "Mod+Shift+Up".move-column-to-workspace-up = { };
+
   ### Workspaces (numbered, i3-style, added on top) ###
   "Mod+1".focus-workspace = 1;
   "Mod+2".focus-workspace = 2;
@@ -48,18 +50,18 @@
   "Mod+Shift+4".move-column-to-workspace = 4;
   "Mod+Shift+5".move-column-to-workspace = 5;
   "Mod+Shift+6".move-column-to-workspace = 6;
-  
+
   ### Window / Column sizing ###
   "Mod+R".switch-preset-column-width = { };
   "Mod+F".maximize-column = { };
   "Mod+Shift+F".fullscreen-window = { };
   "Mod+C".center-column = { };
-    
+
   ### Screenshots ###
   "Print".screenshot = { };
   "Ctrl+Print".screenshot-screen = { };
   "Alt+Print".screenshot-window = { };
-    
+
   ### Session / system ###
   #"Mod+Shift+E".quit = { };
   #"Mod+Escape".toggle-keyboard-shortcuts-inhibit = { allow-inhibiting = false; };
