cd9d23deccf33abd540de8735806e09b669bdedc
Tom <58176147+mxcury@users.noreply.github.com>
2026-07-09 22:41:36 +0100

    Refactor git.nix with enhanced SSH settings
    Updated SSH configuration for better control and security settings.

--- modules/apps/zsh/git.nix
+++ modules/apps/zsh/git.nix
@@ -52,6 +52,21 @@
 
     programs.ssh = {
       enable = true;
+      enableDefaultConfig = false;
+    
+      matchBlocks."*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
+    
       matchBlocks."github.com" = {
         identityFile = "~/.ssh/id_ed25519";
         identitiesOnly = true;
