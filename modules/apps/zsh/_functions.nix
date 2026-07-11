04d7fb389d5d7566cf3de4b42015440b971de90f
Tom <58176147+mxcury@users.noreply.github.com>
2026-07-11 22:02:40 +0100

    Add 'lt' and 'mkcd' functions to zsh functions
    Added new functions 'lt' for listing directories and 'mkcd' for creating and changing to a directory.

--- modules/apps/zsh/_functions.nix
+++ modules/apps/zsh/_functions.nix
@@ -1,3 +1,26 @@
 ''
+  lt() {
+    if [ -z "$1" ]; then
+      eza --tree --group-directories-first --header --total-size --icons
+    else
+      eza --tree --level="$1" --group-directories-first --header --total-size --icons
+    fi
+  }
 
+  # touch that creates parent directories if they don't exist
+  touch() {
+    if [ -n "$1" ]; then
+      mkdir -p -- "$(dirname -- "$1")"
+    fi
+    command touch "$@"
+  }
+
+  # mkdir + cd in one go, registers with zoxide
+  mkcd() {
+    if [ -z "$1" ]; then
+      echo "Usage: mkcd <directory>"
+      return 1
+    fi
+    mkdir -p -- "$1" && cd -- "$1" || return
+  }
 ''
