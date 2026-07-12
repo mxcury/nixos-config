ea931551110e6133fcf3a68debf3a63b2a8bbe2f
Tom <58176147+mxcury@users.noreply.github.com>
2026-07-12 02:20:54 +0100

    Update Brave browser configuration in brave.nix

--- modules/apps/brave.nix
+++ modules/apps/brave.nix
@@ -1,20 +1,35 @@
-{ self, inputs, ...}: {
-  flake.homeModules.brave = { pkgs, ...}: {
+{ self, inputs, ... }: {
+  flake.homeModules.brave = { pkgs, ... }: {
     programs.chromium = {
       enable = true;
       package = pkgs.brave;
+
       commandLineArgs = [
         "--force-dark-mode"
-        "--enable-features=WebUIDarkMode,UseOzonePlatform"
-	    "--ozone-platform-hint=auto"
+        "--enable-features=WebUIDarkMode"
+
+        "--ozone-platform-hint=wayland"
+
+        # skip startup work you don't need
+        "--no-first-run"
+        "--no-default-browser-check"
+        "--disable-background-networking"
+        "--disable-component-update"
+        "--disable-domain-reliability"
+        "--disable-features=Translate,MediaRouter,OptimizationHintsFetching"
       ];
 
+      extraOpts = {
+        "RestoreOnStartup" = 5; # 5 = open to new tab page, 1 = restore previous session
+        "DefaultBrowserSettingEnabled" = false;
+        "MetricsReportingEnabled" = false;
+      };
+
       extensions = [
-        {id = "ghmbeldphafepmbegfdlkpapadhbakde"; } # Proton Pass
-        {id = "epcnnfbjfcgphgdmggkamkmgojdagdnn"; } # UBlock Origin
-        {id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # Sponsor Block
+        { id = "ghmbeldphafepmbegfdlkpapadhbakde"; } # Proton Pass
+        { id = "epcnnfbjfcgphgdmggkamkmgojdagdnn"; } # UBlock Origin
+        { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # SponsorBlock
       ];
     };
   };
-
 }
