77cf69e46331c6ee76e1c16c6e20000b0a481c75
mxcury <mxcury.dev@proton.me>
2026-07-12 13:15:07 +0100

    Add nixos config

---
+++ modules/hosts/nixos/configuration.nix
@@ -0,0 +1,36 @@
+{ self, inputs, ... }: {
+
+  flake.nixosModules.vmConfiguration = { pkgs, lib, ... }: {
+    imports = [
+      self.nixosModules.vmHardware
+      self.nixosModules.common
+    ];
+  
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "/dev/sda";
+    boot.loader.grub.useOSProber = true;
+    boot.loader.grub.fsIdentifier = "provided";
+
+    networking.hostName = "vm";
+
+    programs.nh.flake = "/home/dev/.dotfiles/nixos";
+    environment.sessionVariables = {
+      FLAKE = "/home/dev/.dotfiles/nixos";
+    };
+
+    time.timeZone = "Europe/London";
+
+    i18n.defaultLocale = "en_GB.UTF-8";
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "en_GB.UTF-8";
+      LC_IDENTIFICATION = "en_GB.UTF-8";
+      LC_MEASUREMENT = "en_GB.UTF-8";
+      LC_MONETARY = "en_GB.UTF-8";
+      LC_NAME = "en_GB.UTF-8";
+      LC_NUMERIC = "en_GB.UTF-8";
+      LC_PAPER = "en_GB.UTF-8";
+      LC_TELEPHONE = "en_GB.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
+    };
+  };
+}
