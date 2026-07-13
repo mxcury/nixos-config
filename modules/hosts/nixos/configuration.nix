6e579b50a8137bb311d68605e378cd7b7dd36580
mxcury <mxcury.dev@proton.me>
2026-07-13 18:59:32 +0100

    update lock script

--- modules/hosts/nixos/configuration.nix
+++ modules/hosts/nixos/configuration.nix
@@ -18,8 +18,8 @@
 
     environment.etc."systemd/system-sleep/lock-noctalia" = {
       mode = "0755";
-      source = ./lock-noctalia.sh;
-    };
+      source = pkgs.replaceVars ./lock-noctalia.sh {
+        noctalia_bin = "${self.packages.${pkgs.stdenv.hostPlatform.system}.noctalia-wrapped}/bin/noctalia-shell";
 
     services.xserver.xkb = {
       layout = "gb";
--- modules/hosts/nixos/lock-noctalia.sh
+++ modules/hosts/nixos/lock-noctalia.sh
@@ -3,5 +3,5 @@ if [ "$1" = "pre" ]; then
   USER_ID=$(id -u dev)
   export XDG_RUNTIME_DIR="/run/user/$USER_ID"
   export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
-  su dev -c "qs -c noctalia-shell ipc call lockScreen lock"
+  su dev -c "@noctalia_bin@ ipc call lockScreen lock"
 fi
