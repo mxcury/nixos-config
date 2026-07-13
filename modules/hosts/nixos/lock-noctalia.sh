362093e10abe2952eb172479d7af8884fb067d30
mxcury <mxcury.dev@proton.me>
2026-07-13 19:05:06 +0100

    riddance

--- modules/hosts/nixos/lock-noctalia.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ "$1" = "pre" ]; then
-  USER_ID=$(id -u dev)
-  export XDG_RUNTIME_DIR="/run/user/$USER_ID"
-  export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
-  su dev -c "@noctalia_bin@ ipc call lockScreen lock"
-fi
