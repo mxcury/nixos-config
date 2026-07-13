dbef8119b5c16104d69098f6ede3a2ce8f7339fa
mxcury <mxcury.dev@proton.me>
2026-07-13 22:07:41 +0100

    riddance 2

--- modules/hosts/nixos/configuration.nix
+++ modules/hosts/nixos/configuration.nix
@@ -16,18 +16,6 @@
       FLAKE = "/home/dev/nixos-config";
     };
 
-    environment.etc."systemd/system-sleep/lock-noctalia" = {
-      mode = "0755";
-      source = pkgs.replaceVars ./lock-noctalia.sh {
-        noctalia_bin = "${
-          self.packages.${pkgs.stdenv.hostPlatform.system}.noctalia-wrapped
-        }/bin/noctalia-shell";
-        niri_bin = "${self.packages.${pkgs.stdenv.hostPlatform.system}.niri-wrapped}/bin/niri";
-        id_bin = "${pkgs.coreutils}/bin/id";
-        timeout_bin = "${pkgs.coreutils}/bin/timeout";
-      };
-    };
-
     services.xserver.xkb = {
       layout = "gb";
       variant = "";
