dc5a5fb7ac8a635f5f16f1d386259d178edaa716
mxcury <mxcury.dev@proton.me>
2026-07-13 20:46:28 +0100

    tmp

--- modules/hosts/nixos/lock-noctalia.sh
+++ modules/hosts/nixos/lock-noctalia.sh
@@ -4,6 +4,7 @@ case "$1" in
     USER_ID=$(@id_bin@ -u dev)
     export XDG_RUNTIME_DIR="/run/user/$USER_ID"
     export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
-    /run/wrappers/bin/su dev -c "@noctalia_bin@ ipc call lockScreen lock"
+    export WAYLAND_DISPLAY="wayland-1"
+    timeout 5 @noctalia_bin@ ipc call lockScreen lock
     ;;
 esac
