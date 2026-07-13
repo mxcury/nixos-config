16822a2fd21adebe4b29c982b2820bea08e627b4
Tom <58176147+mxcury@users.noreply.github.com>
2026-07-13 19:41:37 +0100

    Refactor lock-noctalia.sh to use case statement

--- modules/hosts/nixos/lock-noctalia.sh
+++ modules/hosts/nixos/lock-noctalia.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-if [ "$1" = "pre" ]; then
-  USER_ID=$(id -u dev)
-  export XDG_RUNTIME_DIR="/run/user/$USER_ID"
-  export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
-  su dev -c "@noctalia_bin@ ipc call lockScreen lock"
-fi
+case "$1" in
+  pre|post)
+    USER_ID=$(id -u dev)
+    export XDG_RUNTIME_DIR="/run/user/$USER_ID"
+    export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
+    su dev -c "@noctalia_bin@ ipc call lockScreen lock"
+    ;;
+esac
